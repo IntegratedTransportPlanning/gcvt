a0fb8a3c4afdcae746ce6d875191de281512d5b0
Colin Caine <cmc@cs.man.ac.uk>
2018-06-20 14:32:39 +0100

    Code to produce links.gpkg

---
+++ R/process_links.R
@@ -0,0 +1,12 @@
+library(sf)
+
+# Get links with metadata
+nlinks = read_sf("data/sensitive/initial/Network_link.shp", stringsAsFactors = T)
+links = read_sf("data/sensitive/initial/links.geojson")
+nlinks = st_transform(nlinks, st_crs(links))
+links = st_crop(nlinks, links)
+
+# nlinks contains a mix of points, linestrings and multilinestrings.
+# Filter to contain only (multi)linestrings because points will mess us up.
+links = links[grepl("LINESTRING", sapply(st_geometry(links), st_geometry_type)),]
+write_sf(links, "data/sensitive/processed/links.gpkg")
