68d063099ad02ca66af40ee1bd55de5a07b15d12
Colin Caine <cmc@cs.man.ac.uk>
2018-06-20 14:36:06 +0100

    Mkdir first

--- R/process_links.R
+++ R/process_links.R
@@ -6,6 +6,8 @@ links = read_sf("data/sensitive/initial/links.geojson")
 nlinks = st_transform(nlinks, st_crs(links))
 links = st_crop(nlinks, links)
 
+dir.create("data/sensitive/processed", showWarnings = F)
+
 # nlinks contains a mix of points, linestrings and multilinestrings.
 # Filter to contain only (multi)linestrings because points will mess us up.
 links = links[grepl("LINESTRING", sapply(st_geometry(links), st_geometry_type)),]
