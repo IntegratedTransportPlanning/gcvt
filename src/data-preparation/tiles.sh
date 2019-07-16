4cf538d4c0d588dac01e7da5c24843d68c10e8d1
Colin Caine <cmc@cs.man.ac.uk>
2019-07-16 08:09:21 +0100

    Add script to generate MVT tile directories

---
+++ src/data-preparation/tiles.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+USAGE="Usage: $0 /path/to/foo.geojson"
+
+# TODO: Do this from Julia or R (probably R) maybe and with `redo`
+
+set -ex
+
+mkmbtiles() {
+    local geojson="$1"
+    local outdir="$2"
+    local base="$(basename -s .geojson "$geojson")"
+
+    mkdir -p "$outdir"
+    tippecanoe -zg -pC -f "$geojson" -o "$outdir/$base.mbtiles"
+}
+
+mktiledir() {
+    local mbtiles="$1"
+    local outdir="$2"
+    local base="$(basename -s .mbtiles "$mbtiles")"
+
+    mkdir -p "$outdir/tiles"
+    rm -rf "$outdir/tiles/$base"
+    mb-util --image_format=pbf "$mbtiles" "$outdir/tiles/$base"
+}
+
+# Links need to be clipped and stuff, which is done by process_pack_dir at the mo.
+# convert2geojson() {
+#     local geometry=$1
+#     local base=$2
+
+#     Rscript - <<< "
+# library(tidyverse)
+# library(sf)
+
+# links = read_sf('$geometry')
+# links %>%
+#   select(ID_LINK) %>%
+#   write_sf('$base.geojson')
+# "
+# }
+
+[[ $# -lt 1 ]] && (echo "$USAGE"; exit 1)
+
+geometry=$1
+packname=${2-"GCVT_Scenario_Pack"}
+
+outdir="data/processed/$packname/geometry"
+base="${geometry%%.*}"
+
+geom_extension=${geometry#*.}
+
+# if [[ $geom_extension != geojson ]]; then
+#     convert2geojson "$geometry" "$base"
+#     geometry="$base.geojson"
+# fi
+
+mkmbtiles "$geometry" "$outdir"
+mktiledir "$outdir/$base.mbtiles" "$outdir"
