861308971194a96ea365cad8c948a94e35eb3342
Colin Caine <cmc@cs.man.ac.uk>
2019-10-17 19:00:25 +0100

    Small resty server

--- src/appjl/src/appjl.jl
+++ src/appjl/src/appjl.jl
@@ -3,25 +3,75 @@
 module tmp
 
 import Genie
-import Genie.Router: route
+using Genie.Router: route, @params
 
 # This converts its argument to json and sets the appropriate headers for content type
-import Genie.Renderer: json
+using Genie.Renderer: json
+
+import YAML
 
 Genie.config.session_auto_start = false
 
 route("/") do
-  (:message => "Hi there!") |> json
+    (:message => "Hi there!") |> json
 end
 
 
 ### APP ###
 
-# Get list of scenarios (scenario name, id, years active)
+include("src/scenarios.jl")
+links, mats = load_scenarios(packdir)
 
-# Get list of variables (type (od, link, zone))
+# This is unused so far.
+metadata = YAML.load_file(joinpath(packdir, "meta.yaml"))
+
+# Get list of scenarios (scenario name, id, years active)
+# TODO: This does not use the metadata and so doesn't give name.
+list_scenarios() = keys(links)
+
+# Get list of variable names and ids
+# TODO: This does not use the metadata
+function list_variables(domain)
+    if domain == "links"
+        String.(names(first(values(links))))
+    elseif domain == "od_matrices"
+        keys(first(values(mats)))
+    else
+        throw(DomainError(domain, "is invalid"))
+    end
+end
+
+function mat_data(scenario, year, variable)
+    mats[(scenario, year)][variable]
+end
+
+function link_data(scenario, year, variable::String)
+    links[(scenario, year)][Symbol(variable)]
+end
 
 # Get colour to draw each shape for (scenario, year, variable, comparison scenario, comparison year) -> (colours)
+function mat_colour(scenario, year, variable, comparison_scenario, comparison_year)
+    # TODO: palettes, normalisation, etc.
+
+    # dims = 2 sums rows; dims = 1 sums cols
+    sum(mat_data(scenario, year, variable) .- mat_data(comparison_scenario, comparison_year, variable), dims = 2)
+end
+
+route("/scenarios") do
+    list_scenarios() |> json
+end
+
+route("/variables/:domain") do
+    list_variables(@params(:domain)) |> json
+end
+
+route("/data/:domain/:scenario/:year/:variable") do
+    if @params(:domain) == "od_matrices"
+        mat_colour(@params(:scenario), parse(Int, @params(:year)), @params(:variable), "DoNothing", 2025) |> json
+    else
+        throw(DomainError(domain))
+    end
+end
 
 Genie.AppServer.startup(async = false)
 
---
+++ src/appjl/src/scenarios.jl
@@ -0,0 +1,55 @@
+using FileIO: @format_str, load, File
+using RData: RString
+using DataFrames: DataFrame
+
+using Query: @filter
+using Suppressor: @suppress
+
+packdir = "../../data/sensitive/GCVT_Scenario_Pack/"
+
+# This way doesn't emit warnings, but requires some change to output format.
+#= linkscens = load(File(format"RDataSingle", joinpath(packdir, "processed", "link_scenarios.Rds"))) =#
+#= odscens = load(File(format"RDataSingle", joinpath(packdir, "processed", "od_matrices_scenarios.Rds"))) =#
+
+function load_scenarios(packdir=packdir)
+    # This way emits warnings, but works fine.
+    scenarios = @suppress load(File(format"RDataSingle", joinpath(packdir, "processed", "julia_compat_scenarios.Rds")));
+
+    df = DataFrame(values(scenarios), [:scenario, :year, :type, :data]);
+    df.year = parse.(Int, df.year)
+
+    links = df |>
+        @filter(_.type == "links") |>
+        DataFrame |>
+        (df -> Dict((name, year) => data for (name, year, _, data) in zip(eachcol(df)...)));
+
+    mats = df |>
+        @filter(_.type == "od_matrices") |>
+        DataFrame |>
+        (df -> Dict((name, year) => data for (name, year, _, data) in zip(eachcol(df)...)))
+
+    return links, mats
+end
+
+#=
+
+using Statistics
+
+# Aggregate to one df, takes a little while and allocates a lot.
+# But it's only once, so whatever, could do it.
+@time vcat(values(links)...);
+
+# Could just apply the function across an iterable of each thing?
+@time Iterators.flatten(map(df -> @view(df[!, :Cost_Pax]), values(links))) |> mean;
+
+# Same kind of thing for the matrices
+Iterators.flatten(map(dict -> dict["Pax"], values(mats))) |> mean
+
+Dict((name, year) => matrices for (name, year, matrices) in zip(values(odscens)...))
+
+=#
+
+# Interface
+
+# Get a vector or matrix of a variable for a given year and scenario
+# Get a vector of a column for all scenarios and years (maybe not used any more?)
