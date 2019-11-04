076a90607b270eb7e428b4070f2080b2f09bac1b
Oliver Blanthorn <freedom4cows@gmail.com>
2019-11-04 18:33:26 +0000

    Add PoC mapbox viewer

--- src/appjl/src/appjl.jl
+++ src/appjl/src/appjl.jl
@@ -5,6 +5,9 @@ module tmp
 import Genie
 using Genie.Router: route, @params
 
+# Generates HTML responses
+using Genie.Renderer: html
+
 # This converts its argument to json and sets the appropriate headers for content type
 using Genie.Renderer: json
 
@@ -19,7 +22,11 @@ end
 
 ### APP ###
 
+# Mapbox token (set in ENV eventually)
+include("$(@__DIR__)/../.secrets.jl")
+
 include("$(@__DIR__)/scenarios.jl")
+
 links, mats = load_scenarios(packdir)
 
 metadata = YAML.load_file(joinpath(packdir, "meta.yaml"))
@@ -68,6 +75,37 @@ route("/data/:domain/:scenario/:year/:variable") do
     else
         throw(DomainError(domain))
     end
+end
+
+
+route("/map/") do
+    html("""
+        <head>
+        <meta charset='utf-8' />
+        <title>Display a map</title>
+        <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'></script>
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css' rel='stylesheet' />
+        <style>
+        body { margin:0; padding:0; }
+        #map { position:absolute; top:0; bottom:0; width:100%; }
+        </style>
+        </head>
+        <body>
+         
+        <div id='map'></div>
+        <script>
+        mapboxgl.accessToken = '$MAPBOX_TOKEN';
+        var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-74.50, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+        });
+        </script>
+         
+        </body>
+    """)
 end
 
 Genie.AppServer.startup(parse(Int,get(ENV,"GENIE_PORT", "8000")),"0.0.0.0", async = false)
