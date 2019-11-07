a2f184def84c0ae706c6cc1a49562d2e288ced17
Colin Caine <cmc@cs.man.ac.uk>
2019-11-07 15:41:29 +0000

    appts: Use meiosis pattern for state management

--- src/appts/js/index.js
+++ src/appts/js/index.js
@@ -1,141 +1,186 @@
-import mapboxgl from 'mapbox-gl'
-import * as d3 from 'd3'
+import meiosisMergerino from "meiosis-setup/mergerino";
+import simpleStream from "meiosis-setup/simple-stream";
+import merge from "mergerino";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYm92aW5lM2RvbSIsImEiOiJjazJrcjkwdHIxd2tkM2JwNTJnZzQxYjFjIn0.P0rLbO5oj5d3AwpuVqjBSw'
+import mapboxgl from 'mapbox-gl';
+import * as d3 from 'd3';
 
-let queryString = new URLSearchParams(window.location.search)
-let lng = queryString.get("lng") || 32
-let lat = queryString.get("lat") || 48
-let zoom = queryString.get("z") || 4
-
-const map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    center: [lng, lat],
-    zoom: zoom,
-})
+import * as m from 'mithril'
 
 
-const BASEURL = 'http://localhost:2016/'
+// UTILITY FUNCS
 
-function loadLayers() {
-    map.addLayer({
-        id: 'zones',
-        type: 'fill',
-        source: {
-            type: 'vector',
-            tiles: [BASEURL + 'tiles/zones/{z}/{x}/{y}.pbf',],
-            // url: 'http://127.0.0.1:6767/zones.json'
-            // If you don't have this, mapbox doesn't show tiles beyond the
-            // zoom level of the tiles, which is not what we want.
-            maxzoom: 6,
-        },
-        "source-layer": "zones",
-        paint: {
-            'fill-color': 'grey',
-            'fill-outline-color': '#aaa',
-            'fill-opacity': 0.5,
-        },
-        layout: {
-            visibility: 'visible'
+const propertiesDiffer = (props, a, b) =>
+    props.filter(key => a[key] !== b[key]).length !== 0
+
+
+// INITIAL STATE
+
+const initial = (() => {
+    const queryString = new URLSearchParams(window.location.search)
+    const f = key => parseFloat(queryString.get(key))
+    return {
+        lng: f("lng") || 33,
+        lat: f("lat") || 48,
+        zoom: f("zoom") || 4,
+    }
+})()
+
+const mapboxInit = ({lng, lat, zoom}) => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYm92aW5lM2RvbSIsImEiOiJjazJrcjkwdHIxd2tkM2JwNTJnZzQxYjFjIn0.P0rLbO5oj5d3AwpuVqjBSw'
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+        center: [lng, lat],
+        zoom: zoom,
+    })
+
+    // disable map rotation using right click + drag
+    map.dragRotate.disable()
+
+    // disable map rotation using touch rotation gesture
+    map.touchZoomRotate.disableRotation()
+
+    const BASEURL = 'http://localhost:2016/'
+
+    function loadLayers() {
+        map.addLayer({
+            id: 'zones',
+            type: 'fill',
+            source: {
+                type: 'vector',
+                tiles: [BASEURL + 'tiles/zones/{z}/{x}/{y}.pbf',],
+                // url: 'http://127.0.0.1:6767/zones.json'
+                // If you don't have this, mapbox doesn't show tiles beyond the
+                // zoom level of the tiles, which is not what we want.
+                maxzoom: 6,
+            },
+            "source-layer": "zones",
+            paint: {
+                'fill-color': 'grey',
+                'fill-outline-color': '#aaa',
+                'fill-opacity': 0.5,
+            },
+            layout: {
+                visibility: 'visible'
+            }
+        })
+        map.addLayer({
+            id: 'links',
+            type: 'line',
+            source: {
+                type: 'vector',
+                tiles: [BASEURL + 'tiles/links/{z}/{x}/{y}.pbf',],
+                // If you don't have this, mapbox doesn't show tiles beyond the
+                // zoom level of the tiles, which is not what we want.
+                maxzoom: 6,
+            },
+            "source-layer": "links",
+            layout: {
+                'line-cap': 'round',
+                'line-join': 'round',
+                visibility: 'visible',
+            },
+            paint: {
+                'line-opacity': .8,
+                'line-color': 'blue',
+            },
+        })
+    }
+
+    map.on('load', loadLayers)
+
+    return map
+}
+
+// Not in the state because it's it's own state-managing thing.
+const map = mapboxInit(initial)
+
+
+// APP
+
+// https://github.com/foxdonut/meiosis/tree/master/helpers/setup#user-content-application
+const app = {
+    // TODO: populate from query string
+    initial,
+
+    // TODO:
+    // change lat/lon/zoom
+    // receive info for menu
+    // change view params
+    //   scenario
+    //   year
+    //   variable
+    //   change-type
+    //   data-type
+    //   centroids
+    Actions: update => {
+        return {
+            changePosition: (lng, lat, zoom) => {
+                update({lng, lat, zoom})
+            },
         }
-    })
-    map.addLayer({
-        id: 'links',
-        type: 'line',
-        source: {
-            type: 'vector',
-            tiles: [BASEURL + 'tiles/links/{z}/{x}/{y}.pbf',],
-            // If you don't have this, mapbox doesn't show tiles beyond the
-            // zoom level of the tiles, which is not what we want.
-            maxzoom: 6,
+    },
+
+    services: [
+        ({ state, previousState, patch }) => {
+            // Query string updater
+            // take subset of things that should be saved, pushState if any change.
+            const included_in_query = [ "lng", "lat", "zoom" ]
+            let updateRequired = false
+            for (let key of included_in_query) {
+                if (state[key].toPrecision(5) !== previousState[key].toPrecision(5)) {
+                    updateRequired = true
+                    break
+                }
+            }
+
+            if (updateRequired) {
+                const queryString = included_in_query.map(key => `${key}=${state[key].toPrecision(5)}`).join('&')
+                history.pushState({},"", "?" + queryString)
+            }
         },
-        "source-layer": "links",
-        layout: {
-            'line-cap': 'round',
-            'line-join': 'round',
-            visibility: 'visible',
+
+        ({ state, previousState, patch }) => {
+            // Mapbox updater
+            // Update Mapbox's state if it differs from state.
+
+            const mapPos = Object.assign(map.getCenter(), { zoom: map.getZoom() })
+            if (propertiesDiffer(['lng', 'lat', 'zoom'], state, mapPos)) {
+                map.jumpTo({ center: [state.lng, state.lat], zoom: state.zoom })
+            }
         },
-        paint: {
-            'line-opacity': .8,
-            'line-color': 'blue',
-        },
-    })
-}
+    ],
+};
 
-map.on('load', loadLayers)
+const { update, states, actions } =
+    meiosisMergerino({ stream: simpleStream, merge, app });
 
-function moveUpdate(){
-    const cent = map.getCenter()
-    const lnglat = new Map([
-        // Prettier URL at expense of accuracy
-        ["lng",cent.lng.toPrecision(5)],
-        ["lat",cent.lat.toPrecision(5)],
-    ])
-    qsUpdate(lnglat)
-}
-function zoomUpdate(){
-    qsUpdate(new Map([["z",map.getZoom().toPrecision(3)]]))
-}
-function qsUpdate(newkeys){
-    let qs = new URLSearchParams(window.location.search)
-    for (let e of newkeys.entries()) {
-        qs.set(e[0],e[1])
-    }
-    history.pushState({},"", "?" + qs.toString())
-}
 
-// Mapbox IControl - buttons etc will go here
-class HelloWorldControl {
+// VIEWS
 
-    onAdd(map) {
-        this._map = map
-        this._container = document.createElement('div')
-        this._container.className = 'mapboxgl-ctrl'
-        this._container.innerHTML = `
-            <div class="gcvt-ctrl">
-            <h1> What do you want? </h1>
-            <select id="scenario_picker" onchange="gcvt_form_handler(this)">
-                // Might be easier to generate this in JS
-                // the "when do you want your proposal done by"
-                // part needs to ask server what years are possible
-                // so should probably just generate whole thing in JS
-                //$(["<option value='$k'>$(get(v,"name",k))</option>" for (k, v) in metadata["scenarios"]] |> join)
-            </select>
-            </div>
-        `
-        return this._container
+// Console view
+states.map(state => console.log('state', state))
+
+
+// Mapbox action callbacks
+{
+    function positionUpdate() {
+        const cent = map.getCenter()
+        actions.changePosition(cent.lng, cent.lat, map.getZoom())
     }
 
-    onRemove() {
-        this._container.parentNode.removeChild(this._container)
-        this._map = undefined
-    }
+    map.on("moveend", positionUpdate)
+    map.on("zoomend", positionUpdate)
 }
 
-function gcvt_form_handler(element){
-    console.log(element.selectedOptions[0].value)
-    qsUpdate(new Map([["scen",element.selectedOptions[0].value]]))
-}
-map.addControl(new HelloWorldControl())
 
-// Set current setting from URL
-let scen_opts = document.getElementById("scenario_picker")
-let scenario = queryString.get("scen") || "Fleet"
+// Side menu
 
-// NB: this doesn't seem to trigger the onchange handler
-scen_opts.selectedIndex = Array.from(scen_opts.options).findIndex(o => o.value==scenario)
 
-map.on("moveend",moveUpdate)
-map.on("zoomend",zoomUpdate)
- 
-// disable map rotation using right click + drag
-map.dragRotate.disable()
- 
-// disable map rotation using touch rotation gesture
-map.touchZoomRotate.disableRotation()
 
-// Styling demo
+// STYLING
 
 // look at src/app/mb.js for more examples
 
@@ -192,7 +237,7 @@ function normalise(v,bounds,boundtype="midpoint",good="smaller") {
         const tbounds = [...bounds]
         bounds[0] = tbounds[0] - tbounds[1]
         bounds[1] = tbounds[0] + tbounds[1]
-    } 
+    }
     let min = bounds ? bounds[0] : Math.min(...v)
     let max = bounds ? bounds[1] : Math.max(...v)
     if (good == "smaller"){
@@ -236,22 +281,25 @@ async function colourWithMeta(domain,variable,bounds,abs){
         setLinkColours(normalise(data,bounds,abs,dir))
     }
 }
+
 setTimeout(variableTest,2000)
 
 // Link examples
 setTimeout(_ => getData("data?domain=links&year=2030&variable=VCR&scenario=GreenMax").then(x => setLinkColours(normalise(x,[1,0.5],"midpoint","smaller"))),2000)
 
+// const DEBUG = true
+// if (DEBUG)
+    Object.assign(window, {
+        map,
+        update,
+        actions,
+        states,
+        app,
+        m,
 
-// TODO: display colourbar: mapping from original value to colour
+        variableTest,
+        getMeta,
+        colourWithMeta,
 
-const DEBUG = true
-if (DEBUG) {
-    window.getData = getData
-    window.normalise = normalise
-    window.map = map
-    window.setOpacity = setOpacity
-    window.setColours = setColours
-    window.setLinkColours = setLinkColours
-    window.d3 = d3
-    window.variableTest = variableTest
-}
+
+    })
