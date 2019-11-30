c858238ffce1d15c72c184c3a68c4be747f7d721
Oliver Blanthorn <freedom4cows@gmail.com>
2019-11-30 17:07:52 +0000

    Add workaround WordPress plugin

---
+++ src/wordpress-gcvt-plugin/wordpress-gcvt-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: GCVT Map oEmbed whitelister
+ * Plugin URI: https://github.com/IntegratedTransportPlanning/gcvt 
+ * Author: Oliver Blanthorn
+ */
+    wp_oembed_add_provider('http://gcvt.cmcaine.co.uk/map/*','http://gcvt.cmcaine.co.uk/api/oembed');
+?>
