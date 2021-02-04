f8afd1f1155f55deeee92342c7ff9609ae453121
Oliver Blanthorn <freedom4cows@gmail.com>
2021-02-04 18:12:25 +0100

    Remove needless memoize and improve formatting

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -55,8 +55,6 @@ import * as turf from "@turf/turf"
 
 import * as R from "ramda"
 
-import memoize from "fast-memoize"
-
 import ITPLOGO from "../../resources/itp.png"
 import WBLOGO from "../../resources/WBG-Transport-Horizontal-RGB-high.png"
 import KGFLOGO from "../../resources/Korea Green Growth Trust Fund Logo.jpg"
@@ -98,9 +96,13 @@ const nerf = x => (1+erf(x*2-1))/2
 
 // get data from Julia:
 const getData = async endpoint =>
-    (await fetch("/api/" + endpoint + (endpoint.includes("?") ? "&" : "?") + "v=" + (await getApiVersion()))).json().catch(e => console.error(`Error getting data from:\n/api/${endpoint}\n\n`, e))
+    (await fetch(
+        "/api/" + endpoint + (endpoint.includes("?") ? "&" : "?") + "v=" + await API_VERSION_PROMISE)
+    ).json().catch(
+        e => console.error(`Error getting data from:\n/api/${endpoint}\n\n`, e)
+    )
 
-async function _getApiVersion() {
+async function getApiVersion() {
     try {
         return (await (await fetch("/api/version")).json())["version"]
     } catch (e) {
@@ -108,7 +110,7 @@ async function _getApiVersion() {
     }
 }
 
-const getApiVersion = memoize(_getApiVersion)
+const API_VERSION_PROMISE = getApiVersion()
 
 // d3 really doesn't offer a sane way to pick these.
 // Supported list: https://github.com/d3/d3-scale-chromatic/blob/master/src/index.js
